2c0e00044bb902b70b65c147b55014a214557e0d
Bruce Bryant <793041663@qq.com>
2021-04-05 19:52:13 +0800

    ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -3,12 +3,13 @@ const rootDir = process.env.NODE_ENV === "development" ? "src" : "dist";
 
 module.exports = {
   type: "postgres",
+  url: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  synchronize: false,
+  synchronize: true,
   logging: true,
   entities: [rootDir + "/entities/**/*{.ts,.js}"],
   migrations: [rootDir + "/migrations/**/*{.ts,.js}"],
